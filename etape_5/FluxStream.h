b49707b0f22eb7b3f58e332322c1424ac469df9d
Said AHEMT <said.ahemt@nolaroads.com>
2015-01-09 23:58:48 +0100

    adds virtual keyword

--- etape_5/FluxStream.h
+++ etape_5/FluxStream.h
@@ -2,12 +2,12 @@
     #define  FLUX_STREAM_H
     #include <string>
 
-    class FluxStream
+    class                           FluxStream
     {
         public:
-            bool            close();
-            bool            open(std::string&);
-            std::string&    read();
-            int             write(std::string&);
+            virtual bool            open(std::string& filepath);
+            virtual std::string&    read();
+            virtual int             write(std::string& line);
+            virtual bool            close();
     };
 #endif
