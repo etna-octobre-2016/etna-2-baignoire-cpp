1a825db4181a1930583f892c0ab11796a81a79d6
Said AHEMT <said.ahemt@nolaroads.com>
2015-01-09 23:38:48 +0100

    fixes multiple include and std namespace use

--- etape_5/FluxStream.h
+++ etape_5/FluxStream.h
@@ -1,7 +1,13 @@
-class FluxStream
-{
-    virtual bool    close();
-    virtual bool    open(string&);
-    virtual string& read();
-    virtual int     write(string&);
-}
+#ifndef FLUX_STREAM_H
+    #define  FLUX_STREAM_H
+    #include <string>
+
+    class FluxStream
+    {
+        public:
+            virtual bool            close() = 0;
+            virtual bool            open(std::string&) = 0;
+            virtual std::string&    read() = 0;
+            virtual int             write(std::string&) = 0;
+    };
+#endif
