0a2835caeef8b57d069d639be4e02e6a1d9493f3
Said AHEMT <said.ahemt@nolaroads.com>
2015-01-10 17:21:38 +0100

    includes iostream headers and uses std namespace inside (bad practice)

--- etape_5/FluxStream.h
+++ etape_5/FluxStream.h
@@ -1,14 +1,18 @@
 #ifndef FLUX_STREAM_H
     #define  FLUX_STREAM_H
+
+    #include <iostream>
     #include <string>
 
-    class                           FluxStream
+    using namespace std;
+
+    class                   FluxStream
     {
         public:
-            virtual                 ~FluxStream(){}
-            virtual bool            open(std::string& filepath) = 0;
-            virtual std::string&    read() = 0;
-            virtual int             write(std::string& line) = 0;
-            virtual bool            close() = 0;
+            virtual         ~FluxStream(){}
+            virtual bool    open(string& filepath) = 0;
+            virtual string& read() = 0;
+            virtual int     write(string& line) = 0;
+            virtual bool    close() = 0;
     };
 #endif
