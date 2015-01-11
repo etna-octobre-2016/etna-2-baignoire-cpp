4f4c73140cc7b276f013105396819dd568abfcf0
Said AHEMT <said.ahemt@nolaroads.com>
2015-01-11 15:55:58 +0100

    fixes indentation

--- etape_6/FileStreamImpl.cpp
+++ etape_6/FileStreamImpl.cpp
@@ -5,12 +5,12 @@
 
 FileStreamImpl::FileStreamImpl()
 {
-	this->resetAttributes();
+    this->resetAttributes();
 }
 
 FileStreamImpl::~FileStreamImpl()
 {
-	this->close();
+    this->close();
 }
 
 // PUBLIC METHODS
@@ -18,74 +18,74 @@ FileStreamImpl::~FileStreamImpl()
 
 bool FileStreamImpl::close()
 {
-	if (this->file.is_open())
-	{
-		this->file.close();
-		this->resetAttributes();
-		return true;
-	}
-	return false;
+    if (this->file.is_open())
+    {
+        this->file.close();
+        this->resetAttributes();
+        return true;
+    }
+    return false;
 }
 
 bool FileStreamImpl::open(std::string& filePath)
 {
-	this->file.open(filePath, std::ios_base::in | std::ios_base::out | std::ios_base::app);
-	if (this->file.is_open())
-	{
-		this->filePath = filePath;
-		return true;
-	}
-	return false;
+    this->file.open(filePath, std::ios_base::in | std::ios_base::out | std::ios_base::app);
+    if (this->file.is_open())
+    {
+        this->filePath = filePath;
+        return true;
+    }
+    return false;
 }
 
 std::string& FileStreamImpl::read()
 {
-	// static int 				line;
-	// static bool 			eof;
-	// static std::string 		tmp;
-	// static std::ifstream 	file(this->filePath);
-	//
-	// if (file.is_open())
-	// {
-	// 	eof  = true;
-	// 	line = 1;
-	// 	while (std::getline(file, tmp))
-	// 	{
-	// 		std::cout << "line = " << line << " current line = " << this->currentLine << "\n";
-	// 		if (line > this->currentLine)
-	// 		{
-	// 			eof = false;
-	// 			this->currentLine = line;
-	// 			this->currentLineContent = tmp;
-	// 			break;
-	// 		}
-	// 		line++;
-	// 	}
-	// 	if (eof)
-	// 	{
-	// 		this->currentLineContent = "";
-	// 	}
-	// 	file.close();
-	// }
-	return this->currentLineContent;
+    // static int               line;
+    // static bool          eof;
+    // static std::string       tmp;
+    // static std::ifstream     file(this->filePath);
+    //
+    // if (file.is_open())
+    // {
+    //  eof  = true;
+    //  line = 1;
+    //  while (std::getline(file, tmp))
+    //  {
+    //      std::cout << "line = " << line << " current line = " << this->currentLine << "\n";
+    //      if (line > this->currentLine)
+    //      {
+    //          eof = false;
+    //          this->currentLine = line;
+    //          this->currentLineContent = tmp;
+    //          break;
+    //      }
+    //      line++;
+    //  }
+    //  if (eof)
+    //  {
+    //      this->currentLineContent = "";
+    //  }
+    //  file.close();
+    // }
+    return this->currentLineContent;
 }
 
 int FileStreamImpl::write(std::string& line)
 {
-	int lineLength;
-	int writtenCharactersCount;
+    int lineLength;
+    int writtenCharactersCount;
 
-	writtenCharactersCount = 0;
-	if (this->file.is_open())
-	{
-		lineLength = line.length();
-		this->file.write(line.c_str(), lineLength);
-		if (this->file.good())
-		{
-			writtenCharactersCount = lineLength;
-		}
-	}
-	return writtenCharactersCount;
+    writtenCharactersCount = 0;
+    if (this->file.is_open())
+    {
+        lineLength = line.length();
+        this->file.write(line.c_str(), lineLength);
+        if (this->file.good())
+        {
+            writtenCharactersCount = lineLength;
+        }
+    }
+    return writtenCharactersCount;
 }
 
 // PRIVATE METHODS
@@ -93,7 +93,7 @@ int FileStreamImpl::write(std::string& line)
 
 void FileStreamImpl::resetAttributes()
 {
-	this->currentLine = 0;
-	this->currentLineContent = "";
-	this->filePath = "";
+    this->currentLine           = 0;
+    this->currentLineContent    = "";
+    this->filePath              = "";
 }
--- etape_6/FileStreamImpl.h
+++ etape_6/FileStreamImpl.h
@@ -1,32 +1,32 @@
 #ifndef FILE_STREAM_IMPL_H
-	#define  FILE_STREAM_IMPL_H
+    #define  FILE_STREAM_IMPL_H
 
-	#include <fstream>
-	#include "FluxStream.h"
+    #include <fstream>
+    #include "FluxStream.h"
 
-	class        			FileStreamImpl
-	{
-		// ATTRIBUTES
-		private:
-			int 			currentLine;
-			std::string 	currentLineContent;
-			std::string 	filePath;
-			std::fstream	file;
+    class                   FileStreamImpl
+    {
+        // ATTRIBUTES
+        private:
+            int             currentLine;
+            std::string     currentLineContent;
+            std::string     filePath;
+            std::fstream    file;
 
-		// CONSTRUCTOR / DESTRUCTOR
-		public:
-			FileStreamImpl();
-			~FileStreamImpl();
+        // CONSTRUCTOR / DESTRUCTOR
+        public:
+            FileStreamImpl();
+            ~FileStreamImpl();
 
-		// PUBLIC METHODS
-		public:
-			bool            close();
-			bool            open(std::string&);
-			std::string&    read();
-			int             write(std::string&);
+        // PUBLIC METHODS
+        public:
+            bool            close();
+            bool            open(std::string&);
+            std::string&    read();
+            int             write(std::string&);
 
-		// PRIVATE METHODS
-		private:
-			void 			resetAttributes();
-	};
+        // PRIVATE METHODS
+        private:
+            void            resetAttributes();
+    };
 #endif
